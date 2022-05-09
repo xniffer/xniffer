9eac4aad4ed343904b4e3dc962e30994eef2005a
3top1a <3top1a.official@gmail.com>
2022-05-09 20:10:24 +0200

    Restructure, works

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,13 @@
-extern crate rayon;
 extern crate comfy_table;
+extern crate rayon;
 
-use comfy_table::Table;use rayon::prelude::*;
+use comfy_table::Table;
+use rayon::prelude::*;
 use std::error::Error;
 use std::path::PathBuf;
 
 fn main() -> Result<(), Box<dyn Error>> {
-	println!("--- R-liv v{} ---", std::env!("CARGO_PKG_VERSION"));
+	println!("--- xniffer v{} ---", std::env!("CARGO_PKG_VERSION"));
 	let args: Vec<String> = std::env::args().collect();
 
 	if args.len() < 2 {
@@ -14,51 +15,19 @@ fn main() -> Result<(), Box<dyn Error>> {
 	}
 
 	// Assuming the last argument is the file
-	// TODO multiple files
-	let files: Vec<PathBuf> = vec![PathBuf::from(&args[args.len() - 1])];
+	let files_input: Vec<PathBuf> = Vec::from(&args[1..])
+		.into_par_iter()
+		.map(|x| PathBuf::from(x))
+		.collect();
 
 	// Logic
-	let data: Vec<Vec<Data>> = files.par_iter().map(|x| parse(x).unwrap()).collect();
-
-	let mut table = Table::new();
-	table
-		.set_header(vec!["Tag", "Value"])
-	    .load_preset(comfy_table::presets::UTF8_FULL)
-        .apply_modifier(comfy_table::modifiers::UTF8_ROUND_CORNERS)
-        .set_content_arrangement(comfy_table::ContentArrangement::Dynamic)
-        .set_table_width(termsize::get().unwrap().cols);
-
-	for file in data {
-		for entry in file {
-			table.add_row(vec![
-				entry.tag,
-				entry.value.unwrap_or(String::new()),
-			]);
-		}
-	}
-
-    println!("{table}");
+	files_input.par_iter().for_each(|x| parse(x));
 
 	Ok(())
 }
 
-fn parse(path: &PathBuf) -> Result<Vec<Data>, String> {
-	println!("Parsing {}", &path.display());
-
-	/*let file = std::fs::File::open(path).unwrap();
-	let mut bufreader = std::io::BufReader::new(&file);
-	let exifreader = exif::Reader::new();
-	let exif = exifreader.read_from_container(&mut bufreader).unwrap();
-	for f in exif.fields() {
-		println!("{} {} {}",
-				 f.tag, f.ifd_num, f.display_value().with_unit(&exif));
-		Data {
-			tag: f.tag,
-			ifd: f.ifd_num,
-			value: Some(f.to_owned()),
-		};
-	}*/
-
+fn parse(path: &PathBuf) {
+	//println!("Parsing {}", &path.display());
 	let meta = rexiv2::Metadata::new_from_path(&path).unwrap();
 	let mut data: Vec<Data> = Vec::new();
 
@@ -66,40 +35,50 @@ fn parse(path: &PathBuf) -> Result<Vec<Data>, String> {
 	if meta.has_exif() {
 		let exifs = meta.get_exif_tags().unwrap();
 		for e in exifs {
-			data.push(
-				Data {
-					tag: e.clone(),
-					value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string())
-				}
-			);
-		};
+			data.push(Data {
+				tag: e.clone(),
+				value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string()),
+			});
+		}
 	};
 
 	if meta.has_iptc() {
 		let iptcs = meta.get_iptc_tags().unwrap();
 		for e in iptcs {
-			data.push(
-				Data {
-					tag: e.clone(),
-					value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string())
-				}
-			);
-		};
+			data.push(Data {
+				tag: e.clone(),
+				value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string()),
+			});
+		}
 	};
 
 	if meta.has_xmp() {
 		let xmps = meta.get_xmp_tags().unwrap();
 		for e in xmps {
-			data.push(
-				Data {
-					tag: e.clone(),
-					value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string())
-				}
-			);
-		};
+			data.push(Data {
+				tag: e.clone(),
+				value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string()),
+			});
+		}
 	};
 
-	Ok(data)
+	let mut table = Table::new();
+	table
+		.set_header(vec!["Tag", "Value"])
+		.load_preset(comfy_table::presets::UTF8_FULL)
+		.apply_modifier(comfy_table::modifiers::UTF8_ROUND_CORNERS)
+		.set_content_arrangement(comfy_table::ContentArrangement::Dynamic)
+		.set_table_width(
+			termsize::get()
+				.unwrap_or(termsize::Size { rows: 50, cols: 150 })
+				.cols,
+		);
+
+	for entry in data.clone() {
+		table.add_row(vec![entry.tag, entry.value.unwrap_or(String::new())]);
+	}
+
+	println!("{}\n{table}", path.display());
 }
 
 #[derive(std::clone::Clone)]
