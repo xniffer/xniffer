27d2f33383928cf2d55bc7bda12644c9c310eddd
3top1a <3top1a.official@gmail.com>
2022-05-13 10:29:00 +0200

    Colors + unified tag reading

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+extern crate clap;
 extern crate comfy_table;
 extern crate rayon;
-extern crate clap;
 
 use clap::{Arg, Command};
+use colored::Colorize;
 use comfy_table::Table;
 use rayon::prelude::*;
 use std::char::decode_utf16;
@@ -20,27 +21,31 @@ fn main() -> Result<(), Box<dyn Error>> {
 		.version(CARGO_PKG_VERSION)
 		.author(CARGO_PKG_AUTHORS)
 		.about("A simple exif sniffer written in Rust")
-		.arg(Arg::new("PATHS")
-			.required(true)
-			.help("Specify paths")
-			.takes_value(true)
-			.multiple_values(true)
+		.arg(
+			Arg::new("PATHS")
+				.required(true)
+				.help("Specify paths")
+				.takes_value(true)
+				.multiple_values(true),
 		)
-		.arg(Arg::new("RAW")
-			.help("show raw data")
-			.short('r')
-			.long("raw")
-			.takes_value(false)
+		.arg(
+			Arg::new("RAW")
+				.help("show raw data")
+				.short('r')
+				.long("raw")
+				.takes_value(false),
 		)
 		.after_help("https://github.com/3top1a/xniffer")
 		.get_matches();
 
 	let files: Vec<String> = convert_folder_input_into_files_within(
-		matches.values_of_t("PATHS").unwrap_or_else(|e| e.exit())
+		matches.values_of_t("PATHS").unwrap_or_else(|e| e.exit()),
 	);
 
 	// Logic
-	files.par_iter().for_each(|x| parse(x, !matches.is_present("RAW")));
+	files
+		.par_iter()
+		.for_each(|x| parse(x, matches.is_present("RAW")));
 
 	Ok(())
 }
@@ -57,60 +62,41 @@ fn parse(name: &String, show_raw: bool) {
 	let mut data: Vec<Data> = Vec::new();
 
 	// Exif tags
+	// TODO Immutability
+	let mut tags: Vec<String> = Vec::new();
 	if meta.has_exif() {
-		let exifs = meta.get_exif_tags().unwrap();
-		for e in exifs {
-			if meta.get_tag_string(&e).is_err() {
-				continue;
-			}
-
-			let tag = meta.get_tag_string(&e).unwrap_or(String::new()).to_string();
-
-			data.push(Data {
-				tag: e.clone(),
-				value: Some(if tag.len() > 80 && show_raw {
-					if hex::decode(&tag).is_ok() {
-						String::from_utf8(hex::decode(&tag).unwrap())
-							.unwrap_or(truncate(tag.as_ref(), 40).to_owned() + "...")
-					} else if try_string_of_bytes_to_string(&tag).is_ok() {
-						truncate(try_string_of_bytes_to_string(&tag).unwrap().as_ref(), 40).to_owned() + "[r]"
-					} else {
-						truncate(tag.as_ref(), 40).to_owned() + "..."
-					}
-				} else {
-					tag
-				}),
-			});
-		}
+		tags.append(&mut meta.get_exif_tags().unwrap());
 	};
-
 	if meta.has_iptc() {
-		let iptcs = meta.get_iptc_tags().unwrap();
-		for e in iptcs {
-			if meta.get_tag_string(&e).is_err() {
-				continue;
-			}
-
-			data.push(Data {
-				tag: e.clone(),
-				value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string()),
-			});
-		}
+		tags.append(&mut meta.get_iptc_tags().unwrap());
 	};
-
 	if meta.has_xmp() {
-		let xmps = meta.get_xmp_tags().unwrap();
-		for e in xmps {
-			if meta.get_tag_string(&e).is_err() {
-				continue;
-			}
-
-			data.push(Data {
-				tag: e.clone(),
-				value: Some(meta.get_tag_string(&e).unwrap_or(String::new()).to_string()),
-			});
-		}
+		tags.append(&mut meta.get_xmp_tags().unwrap());
 	};
+
+	for e in tags {
+		if meta.get_tag_string(&e).is_err() {
+			continue;
+		}
+
+		let tag = meta.get_tag_string(&e).unwrap_or(String::new()).to_string();
+		data.push(Data {
+			tag: e.clone(),
+			value: Some(if tag.len() > 80 && !show_raw {
+				if hex::decode(&tag).is_ok() {
+					String::from_utf8(hex::decode(&tag).unwrap())
+						.unwrap_or(truncate(tag.as_ref(), 40).to_owned() + &"...".yellow())
+				} else if try_string_of_bytes_to_string(&tag).is_ok() {
+					truncate(try_string_of_bytes_to_string(&tag).unwrap().as_ref(), 40).to_owned()
+						+ &"[r]".yellow()
+				} else {
+					truncate(tag.as_ref(), 40).to_owned() + &"...".yellow()
+				}
+			} else {
+				tag
+			}),
+		});
+	}
 
 	let mut table = Table::new();
 	table
@@ -129,27 +115,28 @@ fn parse(name: &String, show_raw: bool) {
 		);
 
 	for entry in data.clone() {
-		table.add_row(vec![entry.tag, entry.value.unwrap_or(String::new())]);
+		table.add_row(vec![
+			entry.tag.green().to_string(),
+			entry.value.unwrap_or(String::new()),
+		]);
 	}
 
 	println!("{table}");
 }
 
 fn try_string_of_bytes_to_string(s: &String) -> Result<String, u8> {
-	let white_space_seperated = s
-		.split_whitespace()
-		.collect::<Vec<&str>>();
+	let white_space_seperated = s.split_whitespace().collect::<Vec<&str>>();
 
-	let sep: Vec<u16> = white_space_seperated.iter()
-	.map(|f|
-		if f.parse::<u16>().is_err()
-		{
-			0u16
-		}
-		else
-		{f.parse::<u16>().unwrap()}
-	)
-	.collect();
+	let sep: Vec<u16> = white_space_seperated
+		.iter()
+		.map(|f| {
+			if f.parse::<u16>().is_err() {
+				0u16
+			} else {
+				f.parse::<u16>().unwrap()
+			}
+		})
+		.collect();
 
 	let x = decode_utf16(sep).map(|f| f.unwrap()).collect();
 
